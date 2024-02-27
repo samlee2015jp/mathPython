78a98b93d4849997c7f85d227cf1bb3755d1534a
Sam Li <samlee2015jp@gmail.com>
2024-02-28 01:38:32 +0900

    add

---
+++ findLeastCommonMultiple.py
@@ -0,0 +1,19 @@
+import math
+
+print(math.lcm(2,3,6))
+
+def lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    
+    while (True):
+        if (greater % x == 0) and (greater % y == 0):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+print("The LCM is ", lcm(3, 6))
